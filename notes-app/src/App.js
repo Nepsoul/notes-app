3c19a395cb0caf9290d10199a2408d6aaa3b3d21
Niru Magar <nirumagar901@gmail.com>
2022-07-18 13:57:08 +0545

    control component

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Note from "./Note";
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setnewNote] = useState("a new note");
+
+  const addNote = (event) => {
+    event.preventDefault();
+
+    let note = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5 ? true : false,
+    };
+    setNotes([...notes, note]);
+    setnewNote(" ");
+
+    console.log("button clicked", event.target);
+  };
+  const handleNoteChange = (event) => {
+    setnewNote(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">submit</button>
+      </form>
+    </>
   );
-}
+};
 
 export default App;
