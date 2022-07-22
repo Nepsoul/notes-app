a3c1736b3a40bc55413b548efaf2ca33a149513f
Niru Magar <nirumagar901@gmail.com>
2022-07-22 15:24:53 +0545

    feat: .put method

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -2,14 +2,27 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 import Note from "./Note";
+import getAll from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setnewNote] = useState("a new note");
   const [showAll, setshowAll] = useState(true);
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+    console.log(`importance of ${id} needs to be toggled`);
+  };
+
   useEffect(() => {
     axios.get("http://localhost:3001/notes").then((Response) => {
+      //getAll.then((Response) => {
       setNotes(Response.data);
       console.log(Response);
     });
@@ -24,14 +37,21 @@ const App = () => {
       date: new Date().toISOString(),
       important: Math.random() < 0.5 ? true : false,
     };
-    setNotes([...notes, note]);
-    setnewNote(" ");
+    // console.log("button clicked", event.target);
+    // setNotes([...notes, note]);
+    // setnewNote(" ");
 
-    console.log("button clicked", event.target);
+    axios.post("http://localhost:3001/notes", note).then((Response) => {
+      console.log(Response.data);
+      setNotes([...notes, Response.data]);
+      setnewNote(" ");
+    });
   };
+
   const handleNoteChange = (event) => {
     setnewNote(event.target.value);
   };
+
   const random = () => setshowAll(!showAll);
 
   const notesToShow = showAll
@@ -45,7 +65,11 @@ const App = () => {
       <button onClick={random}>show {showAll ? "All" : "Important"}</button>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
--- notes-app/src/Note.js
+++ notes-app/src/Note.js
@@ -1,7 +1,9 @@
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportance }) => {
+  const label = note.toggleImportance;
   return (
     <>
       <li>{note.content}</li>
+      <button onClick={toggleImportance}>show import{label}</button>
     </>
   );
 };
