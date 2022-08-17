88deabca8d944351c6c26fe7bc590d53a7344b82
Niru Magar <nirumagar901@gmail.com>
2022-08-17 12:57:20 +0545

    feat: created backend server using express(), use of REST-Api conventions: get(), post(),delete()

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const App = express();
 App.use(cors());
+App.use(express.json());
 
 let notes = [
   {
@@ -40,9 +41,32 @@ App.get("/notes", (request, response) => {
 });
 
 App.get("/notes/:id", (request, response) => {
-  const currentId = request.params.id;
-  response.send(`The value passed is ${currentId}`);
-  //by this whatever write in after notes/ it will print as it in browser
+  //   console.log(request.params);
+  //   console.log(request.body);
+
+  const currentId = Number(request.params.id);
+  const thisNote = notes.find((note) => note.id === currentId);
+  //   if (thisNote) response.send(thisNote);
+  if (thisNote) response.json(thisNote);
+  else
+    response
+      .status(404)
+      .json({ error: 404, message: `there is no note with id ${currentId}` });
+});
+
+App.delete("/notes/:id", (request, response) => {
+  const currentId = Number(request.params.id);
+  notes = notes.filter((notes) => notes.id !== currentId);
+  response.status(204).end();
+});
+App.post("/notes/", (request, response) => {
+  let myIncomingData = request.body;
+  myIncomingData.id = notes.length + 1;
+  notes.push(myIncomingData);
+
+  //   console.log(request.body);
+  console.log(myIncomingData);
+  response.status(201).json(myIncomingData);
 });
 
 App.listen("3001", () => {
