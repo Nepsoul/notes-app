322cdb76d2239143084367d3ac76ef46a3d525ad
Niru Magar <nirumagar901@gmail.com>
2022-09-01 17:06:03 +0545

    feat: setup of env file (to prevet to push password in Github)

--- part3/notes-server/models/note.js
+++ part3/notes-server/models/note.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const url = process.env.MONGODB_URI;
 
@@ -17,14 +18,6 @@ const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
 });
 
 module.exports = mongoose.model("Note", noteSchema);
