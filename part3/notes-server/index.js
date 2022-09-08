5ce4a9ed75641452d072fe1b3f2c9a51bd7acbce
Niru Magar <nirumagar901@gmail.com>
2022-09-08 16:29:46 +0545

    chore: changing catch error message

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -22,33 +22,6 @@ App.use((request, response, next) => {
 });
 // App.use(express.json());==> if this place at below the app.use give 'undefined', always place at above the code
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy at same time hard",
-//     date: "2022-1-17T17:30:31.098Z",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only JavaScript",
-//     date: "2022-1-17T18:39:34.091Z",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2022-1-17T19:20:14.298Z",
-//     important: true,
-//   },
-//   {
-//     content: "hey there you are lucky",
-//     date: "2022-08-06T05:43:27.539Z",
-//     important: false,
-//     id: 4,
-//   },
-// ];
-
 App.get("/", (request, response) => {
   response.send(response.someThis);
   // response.send("<h1>hello world there</h1>");
@@ -75,16 +48,9 @@ App.get("/notes/:id", (request, response) => {
     })
     .catch((error) => {
       console.log(error);
-      response.status(500).end();
+
+      response.status(400).send({ error: "malformatted id" });
     });
-  // const currentId = Number(request.params.id);
-  // const thisNote = notes.find((note) => note.id === currentId);
-  // //   if (thisNote) response.send(thisNote);
-  // if (thisNote) response.json(thisNote);
-  // else
-  //   response
-  //     .status(404)
-  //     .json({ error: 404, message: `there is no note with id ${currentId}` });
 });
 
 App.delete("/notes/:id", (request, response) => {
