e1e50579e60ee8e2e8d5ada57333a812c858c354
Niru Magar <nirumagar901@gmail.com>
2022-09-12 16:02:19 +0545

    info: adding config

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -9,5 +9,5 @@ const server = http.createServer(app);
 // const PORT = process.env.PORT || "3001";
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${config.PORT}`);
 });
