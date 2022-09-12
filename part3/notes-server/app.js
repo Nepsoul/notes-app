7b12420405013f7ec3ae4ed1e00099e305abcac5
Niru Magar <nirumagar901@gmail.com>
2022-09-12 15:51:44 +0545

    feat: app.js, creating http server from index.js through app.js

---
+++ part3/notes-server/app.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const cors = require("cors");
+
+//importing Note from database through env variable
+const Note = require("./models/note");
+
+const App = express();
+
+//App.use..... is middleware
+App.use(express.static("build"));
+App.use(cors());
+App.use(express.json());
+
+App.use((request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  // response.someThis = "hello world";
+  next();
+});
+// App.use(express.json());==> if this place at below the app.use give 'undefined', always place at above the code
+
+App.get("/", (request, response) => {
+  response.send(response.someThis);
+  // response.send("<h1>hello world there</h1>");
+  // response.setHeader('Content-Type', 'text/html').send("<h1>hello world</h1>")
+});
+App.get("/notes", (request, response) => {
+  //showing accessed note from database
+  Note.find().then((result) => response.json(result));
+  // response.json(notes);
+});
+
+App.get("/notes/:id", (request, response, next) => {
+  //   console.log(request.params);
+  //   console.log(request.body);
+
+  //error handling
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+      //response.status(400).send({ error: "malformatted id" });
+    });
+});
+
+// App.delete("/notes/:id", (request, response) => {
+//   const currentId = Number(request.params.id);
+//   notes = notes.filter((notes) => notes.id !== currentId);
+//   response.status(204).end();
+// });
+App.delete("/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// App.post("/notes/", (request, response) => {
+//   let myIncomingData = request.body;
+//   myIncomingData.id = notes.length + 1;
+//   notes.push(myIncomingData);
+
+//   //   console.log(request.body);
+//   console.log(myIncomingData);
+//   response.status(201).json(myIncomingData);
+// });
+App.post("/notes", (request, response, next) => {
+  const body = request.body;
+  // console.log("body.content is", body.content);
+  // throwing error while content missing
+  if (body.content === "") {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error)); //to catch error
+});
+App.put("/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+App.use((request, response, next) => {
+  response.status(404).send("<h1>No routes found for this request</h1>");
+});
+const errorHandler = (error, request, response, next) => {
+  //console.error(error.message);
+
+  // console.log(error.name);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    //adding validation error
+    console.log("validation error");
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+//  this has to be the last loaded middleware.
+App.use(errorHandler);
+
+// ////if port has given as variable in process.env then use given port otherwise use default port
+// const PORT = process.env.PORT || "3001";
+
+// App.listen(PORT, () => {
+//   console.log(`server listening ${PORT}`);
+// });
