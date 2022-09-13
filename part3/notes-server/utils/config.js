7ecb1d2c8af5c475e0afc8513bd1a9f1c3cd3c1d
Niru Magar <nirumagar901@gmail.com>
2022-09-13 15:03:11 +0545

    info: redefining path in env and assign to port variable

--- part3/notes-server/utils/config.js
+++ part3/notes-server/utils/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 //handaling env variable extracted from seperate file
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; //port defining in env to 3001.
 const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = {
