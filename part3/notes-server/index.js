3855e179b043e7b37a04ba28d2cf8c7f0f7ad222
Niru Magar <nirumagar901@gmail.com>
2022-09-13 15:06:47 +0545

    info: adding comment

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-const server = http.createServer(app);
+const server = http.createServer(app); //creating http server
 
 // //if port has given as variable in process.env then use given port otherwise use default port
 // const PORT = process.env.PORT || "3001";
