21c772ab7a3d1e59fe2ca5dd34ac0afd29c35522
Niru Magar <nirumagar901@gmail.com>
2022-09-14 23:29:18 +0545

    comment

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -6,7 +6,7 @@ const logger = require("./utils/logger");
 const server = http.createServer(app); //creating http server
 
 // //if port has given as variable in process.env then use given port otherwise use default port
-// const PORT = process.env.PORT || "3003";
+// const PORT = process.env.PORT || "3001";
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
