e682644b2168d6fad952f79403f9de71c1c00329
Niru Magar <nirumagar901@gmail.com>
2022-09-23 00:03:58 +0545

    feat: setting up test environment

--- part3/notes-server/utils/config.js
+++ part3/notes-server/utils/config.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 
 //handaling env variable extracted from seperate file
 const PORT = process.env.PORT; //port defining in env to 3001.
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = {
   MONGODB_URI,
