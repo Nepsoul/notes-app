834d6ba73cecbf1409e5b41d09396a1cf8cab608
Niru Magar <nirumagar901@gmail.com>
2022-09-26 15:20:19 +0545

    feat: test_helper file created to refactor test

--- part3/notes-server/tests/note_api.test.js
+++ part3/notes-server/tests/note_api.test.js
@@ -1,30 +1,31 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const helper = require("./test_helper");
 
 const api = supertest(app);
 
 const Note = require("../models/note");
 
-const initialNotes = [
-  {
-    content: "HTML is easy",
-    date: new Date(),
-    important: false,
-  },
-  {
-    content: "Browser can execute only Javascript",
-    date: new Date(),
-    important: true,
-  },
-];
+// const initialNotes = [
+//   {
+//     content: "HTML is easy",
+//     date: new Date(),
+//     important: false,
+//   },
+//   {
+//     content: "Browser can execute only Javascript",
+//     date: new Date(),
+//     important: true,
+//   },
+// ];
 
 beforeEach(async () => {
   //beforeEach function provided by jest library, which runs before test
   await Note.deleteMany({});
-  let noteObject = new Note(initialNotes[0]);
+  let noteObject = new Note(helper.initialNotes[0]);
   await noteObject.save();
-  noteObject = new Note(initialNotes[1]);
+  noteObject = new Note(helper.initialNotes[1]);
   await noteObject.save();
 });
 
@@ -41,7 +42,7 @@ test("all notes are returned", async () => {
   //adding another test
   const response = await api.get("/api/notes");
 
-  expect(response.body).toHaveLength(initialNotes.length); //(response.body=>array), toHaveLength: jest method, checks length
+  expect(response.body).toHaveLength(helper.initialNotes.length); //(response.body=>array), toHaveLength: jest method, checks length
 });
 
 test("a specific note is within the returned notes", async () => {
@@ -63,12 +64,29 @@ test("a valid note can be added", async () => {
     .expect(201)
     .expect("Content-Type", /application\/json/);
 
+  const notesAtEnd = await helper.notesInDb();
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
+
+  const contents = notesAtEnd.map((n) => n.content);
+
+  // const response = await api.get("/api/notes");
+  // const contents = response.body.map((r) => r.content);
+  // expect(response.body).toHaveLength(helper.initialNotes.length + 1);
+
+  expect(contents).toContain("async/await simplifies making async calls");
+});
+
+test("note without content is not added", async () => {
+  //adding another test
+  const newNote = {
+    important: true,
+  };
+
+  await api.post("/api/notes").send(newNote).expect(400);
+
   const response = await api.get("/api/notes");
 
-  const contents = response.body.map((r) => r.content);
-
-  expect(response.body).toHaveLength(initialNotes.length + 1);
-  expect(contents).toContain("async/await simplifies making async calls");
+  expect(response.body).toHaveLength(helper.initialNotes.length);
 });
 
 afterAll(() => {
