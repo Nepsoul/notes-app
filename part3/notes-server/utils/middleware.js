d1ba68b1375915ce9addab39d8756c1895071887
Niru Magar <nirumagar901@gmail.com>
2022-10-10 18:20:56 +0545

    feat: token error handling by extending errorhandler middleware code

--- part3/notes-server/utils/middleware.js
+++ part3/notes-server/utils/middleware.js
@@ -20,6 +20,10 @@ const errorHandler = (error, request, response, next) => {
     //adding validation error
     console.log("validation error");
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({
+      error: "invalid token",
+    });
   }
   next(error);
 };
