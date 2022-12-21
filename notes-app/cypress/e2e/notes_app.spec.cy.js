05eff0d85d8fbbc0f7d55cf001b653c0d0c14476
nepsoul <nirumagar901@gmail.com>
2022-12-21 16:05:03 +0545

    chore: test for login button by its text

--- notes-app/cypress/e2e/notes_app.spec.cy.js
+++ notes-app/cypress/e2e/notes_app.spec.cy.js
@@ -4,4 +4,9 @@ describe("Note app", function () {
     cy.contains("Notes");
     cy.contains("Note App");
   });
+
+  it("login form can be opened", function () {
+    cy.visit("http://localhost:3000");
+    cy.contains("login").click();
+  });
 });
