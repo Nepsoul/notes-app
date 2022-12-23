77e94592da324f21a8519e1548096935c508bfe9
nepsoul <nirumagar901@gmail.com>
2022-12-23 11:37:08 +0545

    chore: test for user login

--- notes-app/cypress/e2e/notes_app.spec.cy.js
+++ notes-app/cypress/e2e/notes_app.spec.cy.js
@@ -1,12 +1,20 @@
 describe("Note app", function () {
-  it("front page can be opened", function () {
+  beforeEach(function () {
     cy.visit("http://localhost:3000");
+  });
+
+  it("front page can be opened", function () {
     cy.contains("Notes");
     cy.contains("Note App");
   });
 
   it("login form can be opened", function () {
-    cy.visit("http://localhost:3000");
     cy.contains("login").click();
+  });
+
+  it("user can login", function () {
+    cy.contains("login").click();
+    cy.get("input:first").type("mluukkai");
+    cy.get("input:last").type("salainen");
   });
 });
